56d0c15d7ea3f1b097966c24b0b40457767f1bc9
ahsananis <ahsananis192@gmail.com>
2023-06-01 23:51:54 +0500

    code new 2

--- src/uitils/homeSliderData-mockup.js
+++ src/uitils/homeSliderData-mockup.js
@@ -8,7 +8,7 @@ export const homeSliderData = [
     {
         backgroundImage: './images/banner-image-3.png',
         smallHeading: "The Ranch",
-        largeHeading: "at birch creek",
+        largeHeading: "at birch creek ",
         bannerPara: "We are happy to answer any questions or inquiries you might have about The Ranch at Birch Creek"
     },
     {
